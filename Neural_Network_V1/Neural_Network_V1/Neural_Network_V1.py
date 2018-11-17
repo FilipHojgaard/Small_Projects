ab80c8e3a059b21d7415de59aad241d3521ddd6b
filiphojgaard <filip.hojgaard@gmail.com>
2018-11-17 12:10:59 +0100

    normalized large input. Neural network V1 seems to work now

--- Neural_Network_V1/Neural_Network_V1/Neural_Network_V1.py
+++ Neural_Network_V1/Neural_Network_V1/Neural_Network_V1.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#  This dataset contains: number of people in household, money for rent, have children? (0 for no, 1 for yes)
+#  This dataset contains: number of people in household, rent, have children? (0 for no, 1 for yes)
 dataset = [[1, 4000, 0], [1, 3000, 0], [2, 5700, 0], [2, 7000, 1], [1, 8000, 1], [2, 12000, 1], [1, 6000, 0], [1, 7040, 1], [2, 14000, 1], [2, 15000, 0]]
 
 
@@ -13,22 +13,31 @@ def sigmoid(value):
 def sigmoid_prime(x):
     return sigmoid(x)*(1-sigmoid(x))
 
+# Input range from 3000 up towards 20000 is way to large for sigmoid as the tangent line is almost horizontal resulting in practically no training.
+# This normalizes the input in a range between 0 and 1
+def normalize(x):
+    return (x-0)/(15000-0)
+
+for i in range(0,len(dataset)):
+    dataset[i][1] = normalize(dataset[i][1])
+
+print(dataset)
+
 w_1 = np.random.normal()
 w_2 = np.random.normal()
 b = np.random.normal()
 
 
-learning_rate = 1
-iterations = 5
+learning_rate = 0.2
+iterations = 5000
 
 for i in range(0,iterations):
     picked = np.random.randint(0,len(dataset))
 
     z = dataset[picked][0]*w_1+dataset[picked][1]*w_2+b
-
     prediction = sigmoid(z)
+    
 
-    print(w_1)
     cost = np.square(prediction-dataset[picked][2])
 
     #print(cost)
@@ -47,9 +56,17 @@ for i in range(0,iterations):
     w_2 = w_2 - learning_rate * d_cost_dw2
     b = b - learning_rate * d_cost_db
 
-mystic_family = [2, 7000]
+mystic_family = [2, 6000]
+mystic_family[1] = normalize(mystic_family[1])
 z = w_1 * mystic_family[0] + w_2 * mystic_family[1] + b
+print("w_1 is: \t\t\t" + str(w_1))
+print("adults in mystic family is: \t" + str(mystic_family[0]))
+print("w_2 is: \t\t\t" + str(w_2))
+print("Mystic family rent: \t\t" + str(mystic_family[1]))
+print("bias is: \t\t\t" + str(b))
+print("z is: \t\t\t\t" + str(z))
 pred = sigmoid(z)
+print("prediciton is: " + str(pred))
 if (pred < 0.5):
     print("Family WITHOUT children predicted")
 else:
