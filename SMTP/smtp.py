da20802924f7e4d37536ef0251e97e07d02ffa4b
filiphojgaard <filip.hojgaard@gmail.com>
2018-11-21 16:11:44 +0100

    Solved the problem with smtp utf-8 encodings.

--- SMTP/smtp.py
+++ SMTP/smtp.py
@@ -1,5 +1,6 @@
 import smtplib
 from  email.parser import Parser
+import datetime
 
 def sendASCII(msg):
     headers = Parser().parsestr('From: <senderadress587@gmail.com>\n'
@@ -18,24 +19,42 @@ def sendASCII(msg):
     mail.close()
 
 
-def sendUTF8(msg):
-    headers = Parser().parsestr('From: <senderadress587@gmail.com>\n'
-            'To: <nalp777@gmail.com>\n'
-            'Subject: test af smtp\n'
-            '\n'
-            +msg+'\n')
+def sendUTF8(message):
+    tls = True
+    sender = "XYZ <senderadress587@gmail.com.com>"
+    to = "ABC <nalp777@gmail.com.com>"
+    subject = "GOOD TEST"
+
+
+    headers = {
+        'Content-Type': 'text/html; charset=utf-8',
+        'Content-Disposition': 'inline',
+        'Content-Transfer-Encoding': '8bit',
+        'From': sender,
+        'To': to,
+        'Date': datetime.datetime.now().strftime('%a, %d %b %Y  %H:%M:%S %Z'),
+        'X-Mailer': 'python',
+        'Subject': subject
+    }
+
+    msg = ''
+    for key, value in headers.items():
+        msg += "%s: %s\n" % (key, value)
+
+    msg += "\n%s\n"  % (message)
 
     receivers = ["nalp777@gmail.com"]
-    mail = smtplib.SMTP("smtp.gmail.com",587)
-    mail.ehlo()
-    mail.starttls()
-    mail.login("senderadress587@gmail.com", "hamsterne")
+    s = smtplib.SMTP("smtp.gmail.com",587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo() # igen? måske vigtigt?
+    s.login("senderadress587@gmail.com", "hamsterne")
     for i in range(0,len(receivers)):
-        mail.sendmail("senderadress587@gmail.com", receivers[i], headers.endcode("utf-8"))
-    mail.close()
+        s.sendmail("senderadress587@gmail.com", receivers[i], msg.encode("utf8"))
+    s.close()
 
 
 # ------------ RUN TESTS
 
 # sendASCII("god test 123")
-sendUTF8("hallo")
+sendUTF8("æøå ÆØÅ")
