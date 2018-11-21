96ae7e90ef3feefa997d17eb97ca28cef5a57754
filiphojgaard <filip.hojgaard@gmail.com>
2018-11-21 15:40:40 +0100

    eventimer added, email sending added, distinct list of emails handled. encoding problem. can only send ascii. æøå not supported

--- sn_ringsted_news/sn_ringsted.py
+++ sn_ringsted_news/sn_ringsted.py
@@ -1,5 +1,49 @@
 from bs4 import BeautifulSoup, Comment
 import requests
+import numpy as np
+import time
+import smtplib
+from email.parser import Parser
+
+def main():
+    old_news = []
+    while(True):
+        # time.sleep(28800)
+        time.sleep(3)
+        old_news = eventTime(old_news)
+
+def eventTime(old_news):
+    new_news = fetchNews()
+    all_news = distinctNews(old_news, new_news)
+    sendMail(all_news)
+    return all_news
+
+def sendMail(news):
+    pass
+    mailText = ""
+    for i in news:
+        mailText = mailText + i + "\n"
+    print(mailText)
+
+    headers = Parser().parsestr('From: <senderadress587@gmail.com>\n'
+        'To: <nalp777@gmail.com>\n'
+        'Subject: sn.dk/ringsted nyheder\n'
+        'Content-Type: text/plain; charset=utf-8\n'
+        '\n'
+        +mailText+'\n')
+
+    receivers = ["nalp777@gmail.com"]
+
+
+    mail = smtplib.SMTP("smtp.gmail.com",587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login("senderadress587@gmail.com", "hamsterne")
+    for i in range(0,len(receivers)):
+        mail.sendmail("senderadress587@gmail.com", receivers[i], headers.as_string())
+
+    mail.close()
+
 
 def fetchNews():
     newsArray = []
@@ -16,7 +60,10 @@ def fetchNews():
 
     return newsArray
 
-# RUN FOR TESTING
-newsArray = fetchNews()
-for i in newsArray:
-    print(i)
+def distinctNews(oldNews, newNews):
+    return list(set(oldNews + newNews))
+
+
+
+
+main()
