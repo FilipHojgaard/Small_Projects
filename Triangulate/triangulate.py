f9deb5642b1f9bf796565a5dc04bc6a99d09bd82
FilipHojgaard <filip.hojgaard@gmail.com>
2018-12-05 09:32:17 +0100

    Triangle made. Displays angles correctly on screen. Screen size increased.

--- Triangulate/triangulate.py
+++ Triangulate/triangulate.py
@@ -3,18 +3,24 @@ import pygame, sys
 from pygame.locals import *
 from math import atan2, degrees, pi
 
-screen_width = 640
-screen_heigh = 480
+screen_width = 1280
+screen_heigh = 720
 
+boat_coords = [400, 150]
+left_point = [100, 650]
+right_point = [600, 650]
 red_x = 500
 red_y = 70
 
 pygame.init()
 fpsClock = pygame.time.Clock()
 windowsSurfaceObj = pygame.display.set_mode((screen_width,screen_heigh))
-pygame.display.set_caption("PIXEL VISUALIZATION")
+pygame.display.set_caption("Triangulate Math Visualisation")
 
 bg_color = pygame.Color(200,200,250)
+
+pygame.font.init()
+myfont = pygame.font.SysFont('Ariel', 27)
 
 def findAngle(x_1, y_1, x_2, y_2, fixed_x, fixed_y):
     angle1 = atan2(y_1 - fixed_y, x_1 - fixed_x)
@@ -22,7 +28,7 @@ def findAngle(x_1, y_1, x_2, y_2, fixed_x, fixed_y):
 
     angle = angle2 - angle1
     angle = degrees(angle)
-    print("Angle between lines is: " +str(angle))
+    return round(angle, 1)
 
 def drawLine(point_a, point_b, COLOR):
     pygame.draw.line(windowsSurfaceObj, COLOR, point_a, point_b, 5)
@@ -30,19 +36,30 @@ def drawLine(point_a, point_b, COLOR):
 
 while(True):
     windowsSurfaceObj.fill(bg_color)
-    drawLine([100, 450], [red_x, red_y], (255, 0, 0))
-    drawLine([100,450], [500, 250], (0, 0, 255))
-    findAngle(red_x, red_y, 500, 250, 100, 450)
+    drawLine(left_point, right_point, (0, 0, 255))  # Left point to right point
+    drawLine(left_point, boat_coords, (255, 0, 0))  # Left point to boat
+    drawLine(right_point, boat_coords, (255, 0, 0)) # Right point to boat
+    left_angle = findAngle(boat_coords[0], boat_coords[1],
+    right_point[0], right_point[1], left_point[0], left_point[1])
+    right_angle = findAngle(boat_coords[0], boat_coords[1],
+    left_point[0], left_point[1], right_point[0], right_point[1])
+    right_angle = round((360 - right_angle), 1)
+
+    left_angle_text = myfont.render(str(left_angle)+u"\u00b0", False, (0,0,0))
+    right_angle_text = myfont.render(str(right_angle)+u"\u00b0", False, (0,0,0))
+
+    windowsSurfaceObj.blit(left_angle_text,(left_point[0]-5,left_point[1]+8))
+    windowsSurfaceObj.blit(right_angle_text,(right_point[0]-5,right_point[1]+8))
 
     keystate = pygame.key.get_pressed()
     if keystate[K_LEFT]:
-        red_x -= 3
+        boat_coords[0] -= 3
     if keystate[K_RIGHT]:
-        red_x += 3
+        boat_coords[0] += 3
     if keystate[K_UP]:
-        red_y -= 3
+        boat_coords[1] -= 3
     if keystate[K_DOWN]:
-        red_y += 3
+        boat_coords[1] += 3
     if keystate[K_ESCAPE]:
         pygame.quit()
         sys.exit()
