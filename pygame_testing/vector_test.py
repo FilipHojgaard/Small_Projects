d4ce420e81cb81261a5074a8776e243ff6c22c1a
filiphojgaard <filip.hojgaard@gmail.com>
2019-03-26 13:49:42 +0100

    Collision function for edge of screen created. Deletes ball objects outside. Made font that shows how many balls there is always. Made it impossible for balls to start with velocity of 0.

--- pygame_testing/vector_test.py
+++ pygame_testing/vector_test.py
@@ -14,6 +14,8 @@ RESOLUTION = (1920, 1080)
 GREEN = pygame.Color(0,255,0)
 WHITE = pygame.Color(255,255,255)
 
+myfont = pygame.font.SysFont('Ariel', 27)
+
 windowSurfaceObj = pygame.display.set_mode(RESOLUTION, pygame.FULLSCREEN)
 pygame.display.set_caption('vector testing')
 
@@ -30,19 +32,25 @@ class Ball():
 
     def __init__(self, *args):
         if (len(args) > 0):
-            print("Bold lavet")
             self.pos = args[0]
-            self.vel = Vector2(random.randint(-2,2), random.randint(-2,2))
+            while(True):
+                self.vel = Vector2(random.randint(-2,2), random.randint(-2,2))
+                if (not(self.vel == Vector2(0,0))):
+                    break
+
             self.Rcolor = random.randint(0,255)
             self.Gcolor = random.randint(0,255)
             self.Bcolor = random.randint(0,255)
             self.color = pygame.Color(self.Rcolor,self.Gcolor,self.Bcolor)
 
+    def collision(self):
+        if (self.pos[0] < 0-self.size) or (self.pos[0] > RESOLUTION[0]+self.size) or (self.pos[1]) < 0-self.size or (self.pos[1] > RESOLUTION[1]+self.size):
+            return True
+
     def random_speed(self):
         self.vel = Vector2(random.randint(-2,2), random.randint(-2,2))
 
     def update(self):
-        print(type((self.vel)[0]))
         self.pos += self.vel
 
 balls = []
@@ -78,10 +86,19 @@ while True:
                 print("RESET")
                 del balls[:]
 
-    for i in range(0, len(balls)):
-        balls[i].update()
-        pygame.draw.circle(windowSurfaceObj, balls[i].color, (int(balls[i].pos[0]), int(balls[i].pos[1])), balls[i].size)
 
+
+    for i in range(len(balls)-1, -1, -1):
+        balls[i].update()
+        if balls[i].collision():
+            del balls[i]
+        else:
+            pygame.draw.circle(windowSurfaceObj, balls[i].color, (int(balls[i].pos[0]), int(balls[i].pos[1])), balls[i].size)
+
+
+
+    number_of_balls_text = myfont.render("Balls: "+str(len(balls)), False, (0,0,0))
+    windowSurfaceObj.blit(number_of_balls_text,(10,10))
 
     pygame.display.update()
     fpsClock.tick(FPS)
