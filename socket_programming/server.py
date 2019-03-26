5677dedaec7a5d2499b0388f2f2487ea62313b02
filiphojgaard <filip.hojgaard@gmail.com>
2019-03-26 15:33:19 +0100

    socket now has a double function that makes the server return whatever number client sent doubled, if it is a number. else it just echoes the text.

--- socket_programming/server.py
+++ socket_programming/server.py
@@ -15,4 +15,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not(data):
                 break
+            if (data.decode('utf-8').isdigit()):
+                number = int(data.decode('utf-8'))
+                number *= 2
+                data = str.encode(str(number))
             conn.sendall(data)
